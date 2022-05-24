03023a08c6975138022c66404b900a9b7f01dbbc
pearsettings44 <jose.a.pereira03@gmail.com>
2022-05-24 23:22:40 +0100

    new files

--- intermediate_html_css/script.js
+++
@@ -1,7 +0,0 @@
-const name_input = document.querySelector("#username");
-const greetMeButton = document.querySelector(".greet-btn");
-const greetingOutput = document.querySelector(".greeting");
-
-greetMeButton.addEventListener("click", (event) => {
-  greetingOutput.innerText = `Hello ${name_input.value}`;
-});

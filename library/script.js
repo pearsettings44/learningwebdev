326bd5a7132c27fcc3233a62590b9ac3cd088ff0
pearsettings44 <jose.a.pereira03@gmail.com>
2022-05-26 17:45:59 +0100

    Add Addbook functions. Need to implement remove book and change status.

--- library/script.js
+++ library/script.js
@@ -1,5 +1,11 @@
 let library = [];
+let number;
 const booksContainer = document.getElementById("books");
+const addBookButton = document.getElementById("addme");
+const titleInput = document.getElementById("title");
+const authorInput = document.getElementById("author");
+const pagesInput = document.getElementById("pages");
+const readInput = document.getElementById("read");
 
 /* Book constructor */
 function Book(title, author, pages, read) {
@@ -14,56 +20,42 @@ Book.prototype.addToLibrary = function () {
   library.push(this);
 };
 
+Book.prototype.changeStus = function () {
+  this.read === "read" ? (this.read = "unread") : (this.read = "read");
+};
+
 /* Displays all books in the library */
 function displayLibrary() {
-  for (i = 0; i < library.length; i++) {
-    let book = library[i];
-    /* Create container for the books's details */
-    let bookDiv = document.createElement("div");
-    /* Create <p> tags with the book's title. author and pages */
-    for (j = 0; j < 3; j++) {
-      element = document.createElement("p");
-      element_content = document.createTextNode(
-        `${book[Object.keys(book)[j]]}`
-      );
-      element.appendChild(element_content);
-      bookDiv.appendChild(element);
-    }
-    /* Create a button that shows the book status (read or not read) */
-    let read = document.createElement("button");
-    let read_text = document.createTextNode(`${book.read}`);
-    read.appendChild(read_text);
-    read.classList.add("button-80");
-
-    bookDiv.appendChild(read);
-    bookDiv.classList.add("book");
-
-    /* Add everything to the DOM */
-    booksContainer.appendChild(bookDiv);
+  let book = library.slice(-1)[0];
+  /* Create container for the books's details */
+  let bookDiv = document.createElement("div");
+  /* Create <p> tags with the book's title. author and pages */
+  for (j = 0; j < 3; j++) {
+    element = document.createElement("p");
+    element_content = document.createTextNode(`${book[Object.keys(book)[j]]}`);
+    element.appendChild(element_content);
+    bookDiv.appendChild(element);
   }
+  /* Create a button that shows the book status (read or not read) */
+  let read = document.createElement("button");
+  let read_text = document.createTextNode(`${book.read}`);
+  read.appendChild(read_text);
+  read.classList.add("button-80");
+
+  bookDiv.appendChild(read);
+  bookDiv.classList.add("book");
+
+  /* Add everything to the DOM */
+  booksContainer.appendChild(bookDiv);
 }
 
-const book1 = new Book("afsfas", "afsfas", 22, "read");
-const book2 = new Book(
-  "harry",
-  "potaaaaaaaaaaaaaaaaaaaaaaaaaaater",
-  60,
-  "unread"
-);
-const book3 = new Book("afsfas", "afsfas", 22, "read");
-const book4 = new Book("harry", "potter", 60, "unread");
-const book5 = new Book("afsfas", "afsfas", 22, "read");
-const book6 = new Book("harry", "potter", 60, "unread");
-const book7 = new Book("afsfas", "afsfas", 22, "read");
-const book8 = new Book("harry", "potter", 60, "unread");
-
-book1.addToLibrary();
-book2.addToLibrary();
-book3.addToLibrary();
-book4.addToLibrary();
-
-book5.addToLibrary();
-book6.addToLibrary();
-book7.addToLibrary();
-book8.addToLibrary();
-displayLibrary();
+/* Add new book and display it */
+addBookButton.addEventListener("click", function () {
+  let title = titleInput.value;
+  let author = authorInput.value;
+  let pages = pagesInput.value;
+  let read = readInput.value;
+  const book = new Book(title, author, pages, read);
+  book.addToLibrary();
+  displayLibrary();
+});

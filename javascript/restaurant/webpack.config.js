8505cd1c348c264fb9ed46fdeebf30d5de091074
pearsettings44 <jose.a.pereira03@gmail.com>
2022-06-03 12:33:58 +0100

    background image

--- javascript/restaurant/webpack.config.js
+++ javascript/restaurant/webpack.config.js
@@ -13,6 +13,11 @@ module.exports = {
 
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+
+        type: "asset/resource",
+      },
     ],
   },
   devtool: "inline-source-map",

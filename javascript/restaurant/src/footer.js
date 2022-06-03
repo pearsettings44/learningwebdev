b65f22ded3370713e619914c11e626cfdeda29eb
pearsettings44 <jose.a.pereira03@gmail.com>
2022-06-03 23:13:28 +0100

    Add comments.

--- javascript/restaurant/src/footer.js
+++ javascript/restaurant/src/footer.js
@@ -2,6 +2,7 @@ import { createHTMLelement } from "./createElements";
 import { maindiv } from "./index";
 import Icon from "./img/github-icon-white-6.png";
 
+/* Creates a footer with github username and icon */
 function renderfooter() {
   const footer = createHTMLelement("footer", "footer");
   const paragraph = createHTMLelement(
@@ -11,13 +12,16 @@ function renderfooter() {
     "Copyright pearsettings44"
   );
   const githublink = createHTMLelement("a");
+  /* github profile url*/
   githublink.href = "https://github.com/pearsettings44";
+  /* github icon */
   const gitIcon = createHTMLelement("img", "git");
   gitIcon.src = Icon;
   gitIcon.alt = "github icon";
   footer.appendChild(paragraph);
   githublink.appendChild(gitIcon);
   footer.appendChild(githublink);
+  /* Append footer to the DOM */
   maindiv.appendChild(footer);
 }
 
--- javascript/restaurant/src/navbar.js
+++ javascript/restaurant/src/navbar.js
@@ -1,13 +1,16 @@
 import { createHTMLelement } from "./createElements";
 import { maindiv } from "./index";
 
+/* Creates a navbar with Home, Menu and About pages */
 function rendernav() {
   const navbar = createHTMLelement("nav", "navbar");
   const items = ["home", "menu", "about"];
+  /* Create each page */
   items.forEach((item) => {
     const list_item = createHTMLelement("a", "", [item], `${item}`);
     navbar.appendChild(list_item);
   });
+  /* Append to the DOM */
   maindiv.appendChild(navbar);
 }
 

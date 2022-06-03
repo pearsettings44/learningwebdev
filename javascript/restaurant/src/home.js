847c056d0174b0016051ba0d148cc47a32f597f8
pearsettings44 <jose.a.pereira03@gmail.com>
2022-06-03 23:42:39 +0100

    finish home page

---
+++ javascript/restaurant/src/home.js
@@ -0,0 +1,26 @@
+import { createHTMLelement } from "./createElements";
+import { maindiv } from "./index";
+
+/* Creates the home page */
+function renderhome() {
+  const homeholder = createHTMLelement("div", "home");
+  const mainheader = createHTMLelement(
+    "h1",
+    "mainheader",
+    "",
+    "Come on down for some delicious cousine!"
+  );
+  const secondheader = createHTMLelement(
+    "h2",
+    "secondheader",
+    "",
+    "Tasty and affordable!"
+  );
+  const orderbutton = createHTMLelement("button", "ordernow", "", "Order Now!");
+  homeholder.appendChild(mainheader);
+  homeholder.appendChild(secondheader);
+  homeholder.appendChild(orderbutton);
+  maindiv.appendChild(homeholder)
+}
+
+export { renderhome };
--- javascript/restaurant/src/index.js
+++ javascript/restaurant/src/index.js
@@ -1,7 +1,12 @@
 import { rendernav } from "./navbar";
 import { renderfooter } from "./footer";
+import { renderhome } from "./home";
 
 const maindiv = document.getElementById("content");
-rendernav();
-renderfooter();
+function homepage() {
+  rendernav();
+  renderhome();
+  renderfooter();
+}
+homepage()
 export { maindiv };
